533fa4699892e1d923e6c91ec65c64cf12c7988b
Marcus20119 <hoanglongnguyenhuu20119@gmail.com>
2023-04-10 17:12:46 +0700

    update api

---
+++ .history/pages/api/[...path]_20230410170200.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxy from 'http-proxy';
+
+// type Data = {
+//   name: string;
+// };
+
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  // Vì đã để token bên trong header nên không cần gửi cookie lên server nữa
+  req.headers.cookie = '';
+
+  proxy.web(req, res, {
+    target: 'https://js-post-api.herokuapp.com',
+    changeOrigin: true, // Đổi localhost:3000 thành target phía trên
+    selfHandleResponse: false, // Nếu set = true thì proxy sẽ xử lý response trước khi trả về cho client, còn set = false thì trả về thẳng
+  });
+
+  // res.status(200).json({ name: 'John Doe' });
+}
